255becf9e2b505bdc90fd861e9100f37086a0c23
Tran Dinh Nguyen <trandinhnguyen46159@gmail.com>
2023-12-27 00:47:02 +0700

    add demo_3 folder

---
+++ SpringBoot_And_Hibernate/src/demo_3/demo-dependency/src/main/java/vn/nguyentd/spring/demodependency/DemoDependencyApplication.java
@@ -0,0 +1,13 @@
+package vn.nguyentd.spring.demodependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDependencyApplication.class, args);
+	}
+
+}
